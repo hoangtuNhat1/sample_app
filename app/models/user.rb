de149dd1069b463a8eeef233559fbbd86870b3aa
hoangtuNhat1 <kimchanbn123@gmail.com>
2024-10-28 10:41:24 +0700

    Chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
   before_save :downcase_email
+  validates :name, presence: true,
+                   length: {maximum: Settings.max_name_length}
 
-  validates :name, presence: true, length:
-  {maximum: ENV["MAX_NAME_LENGTH"].to_i}
-  validates :email, presence: true, format: {with: URI::MailTo::EMAIL_REGEXP},
-  length: {maximum: ENV["MAX_EMAIL_LENGTH"].to_i}, uniqueness: true
+  validates :email, presence: true,
+                    format: {with: URI::MailTo::EMAIL_REGEXP},
+                    length: {maximum: Settings.max_email_length},
+                    uniqueness: true
+
   validates :password, presence: true,
-length: {minimum: ENV["MIN_PASSWORD_LENGTH"]}, if: :password
+                       length: {minimum: Settings.min_password_length},
+                       if: :password
 
   has_secure_password
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
     get "static_pages/home", to: "static_pages#home"
     get "static_pages/help", to: "static_pages#help"
     root "static_pages#home"
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: %i(new create show)
   end
 end
