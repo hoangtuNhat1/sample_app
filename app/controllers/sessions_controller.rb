2030d4346c1ec4cd598a4dfe769e421a71704e87
hoangtuNhat1 <kimchanbn123@gmail.com>
2024-11-05 11:10:53 +0700

    Chapter 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      reset_session
+      log_in @user
+      redirect_to @user
+    else
+      flash.now[:danger] = t("new_session.invalid")
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = t "new_user.success"
       redirect_to @user
     else
