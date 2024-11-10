e46bec0c416d375e73be6bc60e5fdde11c3d29db
hoangtuNhat1 <kimchanbn123@gmail.com>
2024-11-11 03:09:03 +0700

    Chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,23 @@
 class UsersController < ApplicationController
-  USER_PARAMS = [:name, :email, :password, :password_confirmation].freeze
+  before_action :logged_in_user, only: %i(index edit update destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+  before_action :find_user, only: %i(show edit update destroy)
+  def index
+    @pagy, @users = pagy(User.all, items: Settings.item_per_page)
+  end
 
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
+  def show; end
 
-    flash[:warning] = t "new_user.warning"
-    redirect_to root_path
+  def edit; end
+
+  def update
+    if @user.update user_params
+      flash[:success] = t "update_user.success"
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def new
@@ -16,17 +27,48 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
+      reset_session
       flash[:success] = t "new_user.success"
+      log_in @user
       redirect_to @user
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    flash[:success] = t "destroy.success"
+    redirect_to users_url, status: :see_other
+  end
+
   private
 
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
+  end
+
   def user_params
-    params.require(:user).permit(*USER_PARAMS)
+    params.require(:user).permit(User::USER_PARAMS)
+  end
+
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = t "authorization.login"
+    redirect_to login_url, status: :see_other
+  end
+
+  def correct_user
+    find_user
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+
+    flash[:warning] = t "flash.warning"
+    redirect_to root_path
   end
 end
